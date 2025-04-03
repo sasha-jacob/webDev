dce5d33e22a4ed44865b8401bfcf2159ffde62aa
sasha-jacob <sj4703@nyu.edu>
2025-04-03 00:01:26 -0400

    week10 notes and hw

---
+++ Week10/fundamentals/fundaHW.js
@@ -0,0 +1,45 @@
+//Task 1 - Hello World
+alert("Hello World!");
+console.log("My first JavaScript assignment!");
+document.getElementById("output").innerHTML = "I just modified this element with JavaScript";
+
+//Task 2 - Variable Practice
+var firstName = "Sasha";
+var age = 18;
+var favNumber = 28.69;
+console.log("My name is " + firstName + ", I am " + age + " years old, and my favorite number is " + favNumber + ".")
+
+//Task 3 - Calculator Function
+function calculate(num1,num2){
+    var sum = num1 + num2;
+    var product = num1*num2;
+    var quotient = num1 / num2;
+    var difference = num1 - num2; 
+    console.log("The sum is " + sum);
+    console.log("The product is " + product);
+    console.log("The quotient is " + quotient);
+    console.log("The difference is " + difference);
+}
+
+calculate(21,12);
+calculate(28,69);
+calculate(25,45);
+
+//Task 4 - Temperature Converter
+function fahrenheitToCelsius(fValue){
+ let celsiusFormula = (fValue - 32) * (5 / 9); 
+}
+fahrenheitToCelsius(10);
+
+function celsiusToFahrenheit(cValue){
+ let fahrenheitFormula = (cValue * 9 / 5) + 32;
+}
+celsiusToFahrenheit(15);
+
+//Task 5 - String Manipulation
+
+//Task 6 - Toggle Switch
+
+//Task 7 - Counting Loop
+
+//Bonus Challenge - Contact Forms
---
+++ Week10/introNotes.js
@@ -0,0 +1,22 @@
+console.log("this is my first console log!");
+// Write an alert that says "Hello World"
+alert("Hello World");
+
+// Console log "Hello World"
+console.log("Hello World!");
+
+// Change html element
+document.getElementById
+
+// Write a statement that adds 5 + 6
+//var or let works as well
+//its not python, so no need to define int, var, float, str, etc.
+var x = 5;
+var y = 6;
+var z = x + y;
+
+//Or 
+function sumValues(5,6) {
+    var sum = a + b;
+    console.log(a + "+" + b + "=" + sum + ".");
+  }
